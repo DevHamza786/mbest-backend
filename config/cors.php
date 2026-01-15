07de30b3d6e3e5308d8b55eb7a5dc027117869cd
HamzaDev786 <muhammadhhamzamehmood@gmail.com>
2026-01-15 11:19:32 +0500

    cros.php

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,7 @@ return [
         'http://localhost:8080',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:8080',
+        'https://mbest-lms.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
